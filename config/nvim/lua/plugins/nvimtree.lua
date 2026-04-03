891ecf40f47a82b76e4bfd63c4442bf73b11b015
lucasssoh <ramilisonlucasmahatoky@gmail.com>
2026-04-03 12:42:15 +0200

    change nvimtree bug 1

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -3,15 +3,41 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("nvim-tree").setup({
-            view = { width = 30 },
-            filters = { dotfiles = false, custom = {} },
-            git = { ignore = false },
+            -- Surveillance système pour le rafraîchissement auto
+            filesystem_watchers = { enable = true },
+            
+            renderer = {
+                icons = {
+                    show = {
+                        git = true, -- Active les icônes Git
+                        file = true,
+                        folder = true,
+                        folder_arrow = true,
+                    },
+                    glyphs = {
+                        git = {
+                            unstaged = "✗",
+                            staged = "✓",
+                            unmerged = "",
+                            renamed = "➜",
+                            untracked = "★",
+                            deleted = "",
+                            ignored = "◌",
+                        },
+                    },
+                },
+            },
+            git = {
+                enable = true,
+                ignore = false, -- Pour voir aussi les icônes sur les fichiers ignorés
+                show_on_dirs = true, -- Très pratique : l'icône remonte sur les dossiers parents
+            },
         })
 
-        -- Refresh automatique quand on revient dans nvim
+        -- Le petit hack pour forcer Git à se rafraîchir quand tu reviens sur Neovim
         vim.api.nvim_create_autocmd("FocusGained", {
             callback = function()
-                require("nvim-tree.api").tree.reload()
+                require("nvim-tree.api").git.reload()
             end,
         })
     end,
