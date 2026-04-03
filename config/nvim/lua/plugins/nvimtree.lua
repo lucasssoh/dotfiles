3bccabc9cb4bf1b0b3edcca3120ab1a144cced85
lucasssoh <ramilisonlucasmahatoky@gmail.com>
2026-04-03 12:37:01 +0200

    change nvim tree bug 0

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -7,5 +7,12 @@ return {
             filters = { dotfiles = false, custom = {} },
             git = { ignore = false },
         })
+
+        -- Refresh automatique quand on revient dans nvim
+        vim.api.nvim_create_autocmd("FocusGained", {
+            callback = function()
+                require("nvim-tree.api").tree.reload()
+            end,
+        })
     end,
 }
