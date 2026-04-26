a5d6903fe0b35e8846999185bb43284300594b2f
lucasssoh <ramilisonlucasmahatoky@gmail.com>
2026-04-26 12:00:32 +0200

    autopairs in nvim

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -3,24 +3,35 @@ return {
     event = "InsertEnter",
     config = function()
         local autopairs = require("nvim-autopairs")
+        local Rule = require('nvim-autopairs.rule')
 
         autopairs.setup({
-            check_ts = true, -- Utilise Treesitter pour être plus intelligent
+            check_ts = true, 
             ts_config = {
-                lua = { "string" }, -- Ne pas ajouter de paires dans les chaînes de caractères Lua
+                lua = { "string" }, 
                 javascript = { "template_string" },
             },
-            -- Désactive l'ajout de paire si le curseur est juste devant un caractère alphanumérique
-            ignored_next_char = [=[[%w%%%'%[%"%.%]]=],
+            -- On simplifie ici pour permettre la fermeture plus souvent
+            ignored_next_char = "[%w%.]", 
+            -- Force la fermeture même s'il y a des espaces après
+            enable_check_bracket_line = false, 
         })
 
-        -- --- LA MAGIE DE L'INDENTATION (TOUCHE ENTRÉE) ---
-        -- Cette partie permet de faire : { + Entrée -> {
-        --                                           | (curseur ici)
-        --                                         }
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        -- --- AJOUTER LES RÈGLES SPÉCIFIQUES ---
+        -- Cela force l'ajout de l'accolade même dans des conditions strictes
+        autopairs.add_rules({
+          Rule("{ ", " }")
+            :with_pair(function() return false end)
+            :with_move(function(opts)
+                return opts.prev_char:match(".%}") ~= nil
+            end)
+            :use_key("}"),
+        })
+
+        -- Intégration avec nvim-cmp (inchangé)
         local cmp_status, cmp = pcall(require, "cmp")
         if cmp_status then
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
             cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end
     end,
