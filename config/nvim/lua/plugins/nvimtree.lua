0df1e6e2959ee516b4da317f86831fb91e7acbb2
lucasssoh <ramilisonlucasmahatoky@gmail.com>
2026-04-27 23:00:25 +0200

    Add tab map for file opening

--- config/nvim/lua/plugins/nvimtree.lua
+++ config/nvim/lua/plugins/nvimtree.lua
@@ -2,7 +2,23 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        local function my_on_attach(bufnr)
+            local api = require("nvim-tree.api")
+
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            -- Appliquer les raccourcis par défaut
+            api.config.mappings.default_on_attach(bufnr)
+
+            -- Configurer TAB pour ouvrir le fichier ou dossier (comme Entrée)
+            -- On écrase le mapping "Preview" par défaut
+            vim.keymap.set('n', '<Tab>', api.node.open.edit, opts('Open'))
+        end
+
         require("nvim-tree").setup({
+            on_attach = my_on_attach,
             filesystem_watchers = { enable = true },
             renderer = {
                 icons = {
