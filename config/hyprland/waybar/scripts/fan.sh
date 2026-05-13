16c65efb569d8ec041affd798c3950b9d001adad
lucasssoh <ramilisonlucasmahatoky@gmail.com>
2026-05-13 22:46:19 +0200

    fix the thing on hyprland

--- config/hyprland/waybar/scripts/fan.sh
+++ config/hyprland/waybar/scripts/fan.sh
@@ -4,7 +4,9 @@ FAN_PATH=$(find /sys/class/hwmon/hwmon*/fan1_input 2>/dev/null | head -n 1)
 
 if [ -f "$FAN_PATH" ]; then
     RPM=$(cat "$FAN_PATH")
-    echo " ${RPM} RPM"
+
+    formatted_rpm=$(printf "%4d" "$RPM")
+    echo " ${formatted_rpm} RPM"
 else
     echo " N/A"
 fi
