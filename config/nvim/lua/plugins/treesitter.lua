5794957723aa989480d4f1f10182aa4d9ecb43d0
lucasssoh <ramilisonlucasmahatoky@gmail.com>
2026-08-04 15:58:44 +0200

    Fix Comment plugin for neovim

---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+local parsers = {
+  "lua", "javascript", "typescript", "tsx", "python",
+  "java", "json", "markdown", "markdown_inline",
+  "bash", "html", "css", "yaml", "vim", "vimdoc",
+}
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  build = ":TSUpdate",
+  lazy = false,
+  config = function()
+    require("nvim-treesitter").setup()
+    require("nvim-treesitter").install(parsers)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        vim.treesitter.start()
+      end,
+    })
+  end,
+}
