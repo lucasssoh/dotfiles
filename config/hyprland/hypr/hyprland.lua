757e52902290e8605c5f1b8f8d51a5b57759055c
lucasssoh <ramilisonlucasmahatoky@gmail.com>
2026-09-15 00:31:33 +0200

    Change cursor speed from 0 to 0.3

--- config/hyprland/hypr/hyprland.lua
+++ config/hyprland/hypr/hyprland.lua
@@ -179,7 +179,7 @@ hl.config({
         follow_mouse = 1,
         repeat_rate = 45,
         repeat_delay = 200,
-        sensitivity  = 0,
+        sensitivity  = 0.3,
         touchpad = {
             natural_scroll       = true,
             tap_to_click         = true,
